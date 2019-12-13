7dc427a92fcc19a96fec6399a0a974565b0042ac
TAC <tac@tac42.net>
2019-12-14 01:14:24 +0900

    Add requests fixture

--- tests/test_yasuri_text_node.py
+++ tests/test_yasuri_text_node.py
@@ -9,9 +9,13 @@ import threading
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 # ---------------------------------------------------------------------
-@pytest.fixture(scope='function', autouse=True)
-def scope_function():
-    yield
+URL = "http://localhost:8888/tests/htdocs/index.html"
+
+
+@pytest.fixture(scope='function')
+def index_html():
+    html = requests.get(URL)
+    return html
 
 
 def start_server(server):
@@ -37,14 +41,22 @@ def scope_session():
     thread.join()
 
 
-def test_scrape_text_hello_yasuri():
+# ---------------------------------------------------------------------
+def test_scrape_text_hello_yasuri(index_html):
     """
     'scrape text text <p>Hello,Yasuri</p>'
     """
 
-    url = "http://localhost:8888/tests/htdocs/index.html"
-    html = requests.get(url)
-
     node = TextNode(path='p:nth-child(1)')
-    actual = node.inject(html.text)
+    actual = node.inject(index_html.text)
     assert "Hello,Yasuri" == actual
+
+
+def test_return_empty_text_if_no_match_node(index_html):
+    """
+    return empty text if no match node
+    """
+
+    node = TextNode(path='no_match_node')
+    actual = node.inject(index_html.text)
+    assert "" == actual
